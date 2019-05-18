aa3153b6ec4f1fbb6367b4c233d1344f36292c40
Grzegorz Janik <45257536+simplycad@users.noreply.github.com>
2019-05-18 12:26:19 +0200

    Update Konwerterhex_bin.cpp

--- Konwerterhex_bin.cpp
+++ Konwerterhex_bin.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
 	
-	int n;
+	int n, maska;
 	int hex[16]={0};
 	string lhex[]={"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 	string ldec[]={"0000", "0001","0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010","1011","1100","1101","1110","1111"};
@@ -14,24 +14,39 @@ int main(){
 	
 	cout<<"Podaj n: "<<endl;
 	cin>>n;
+
+// Wyswietlanie wartosci bitowej
+		cout<<"Wartosc bitu: "<<endl;
+	
+	for(int i=31; i>=0; i--){
+		
+		maska=(1<<i);
+		if((n&maska)!=0)
+			cout<<"1";
+			else
+			cout<<"0";		
+	}
+	
 	
 	for(int i=0; i<8; i++)	{
 		
 		hex[i]=n%16;
 		n=n/16;
 	}
+	cout<<endl;
 	cout<<"Liczba w systemie szesnastkowym"<<endl;
 	cout<<"0x";
 	
-	for(int i=7; i>=0; i--)	{	
+	for(int i=7; i>=0; i--)	
 	cout<<lhex[hex[i]];
-}
+
 	
 	cout<<endl;
 	cout<<"Liczba w systemie binarnym"<<endl;
 
 	for(int i=7; i>=0; i--)	
 		cout<<ldec[hex[i]]<<" ";
-	
+
+
 	return 0;
 }
